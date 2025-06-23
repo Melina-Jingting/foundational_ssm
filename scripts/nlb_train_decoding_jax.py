2e5b4dd5c3f14479f15dafa50275d30661b4df39
Melina-Jingting <melinajingting@gmail.com>
2025-06-23 17:31:44 +0100

    allow frozen ssm or mlp

--- scripts/nlb_train_decoding_jax.py
+++ scripts/nlb_train_decoding_jax.py
@@ -16,34 +16,14 @@ from jax.tree_util import tree_map, tree_flatten_with_path
 import equinox as eqx
 import optax
 
-from omegaconf import OmegaConf
-
 from foundational_ssm.models import S5
 from foundational_ssm.utils import h5_to_dict
 from foundational_ssm.data_preprocessing import smooth_spikes
 
-class RNN(eqx.Module):
-    hidden_size: int
-    cell: eqx.Module
-    linear: eqx.nn.Linear
-    bias: jax.Array
+import hydra
+from omegaconf import DictConfig, OmegaConf
+from hydra.utils import to_absolute_path
 
-    def __init__(self, in_size, out_size, hidden_size, *, key):
-        ckey, lkey = random.split(key)
-        self.hidden_size = hidden_size
-        self.cell = eqx.nn.GRUCell(in_size, hidden_size, key=ckey)
-        self.linear = eqx.nn.Linear(hidden_size, out_size, use_bias=False, key=lkey)
-        self.bias = jnp.zeros(out_size)
-
-    def __call__(self, input, state, key):
-        hidden = jnp.zeros((self.hidden_size,))
-
-        def f(carry, inp):
-            return self.cell(inp, carry), None
-
-        out, _ = jax.lax.scan(f, hidden, input)
-        # sigmoid because we're performing binary classification
-        return jax.nn.sigmoid(self.linear(out) + self.bias), state
 
 @eqx.filter_jit
 def predict_batch(model, state, inputs, key):
@@ -108,6 +88,7 @@ def compute_metrics(preds, targets):
         "mse": mse
     }
 
+
 def log_model_params_and_grads_wandb(model, grads=None):
     model_params = tree_flatten_with_path(model)[0] 
     grads = tree_flatten_with_path(grads)[0] if grads is not None else []
@@ -125,52 +106,54 @@ def log_model_params_and_grads_wandb(model, grads=None):
             wandb.log({
                 f"grads/{full_path}": hist
             })
+            
+def freeze_filter(path, value, freeze_ssm=False, freeze_linear=False):
+    if freeze_ssm and "ssm" in path:
+        return False
+    if freeze_linear and "linear" in path:
+        return False
+    return eqx.is_inexact_array(value)
 
 @eqx.filter_jit
-def mse_loss(model, state, inputs, targets, key):
-    # model = eqx.combine(model_params, model_static)
+@eqx.filter_value_and_grad(has_aux=True)
+def mse_loss(model_params, model_static, state, inputs, targets, key):
+    model = eqx.combine(model_params, model_static)
     batch_keys = random.split(key, inputs.shape[0])
     preds, state = jax.vmap(model, axis_name="batch", in_axes=(0, None, 0), out_axes=(0, None))(inputs, state, batch_keys)
     mse = jnp.mean((preds - targets) ** 2)
     return (mse, state)
 
 @eqx.filter_jit
-def make_step(model, inputs, targets, state, opt, opt_state, key):
-    # filter_spec = jax.tree_util.tree_map(lambda _: True, model)
-    # model_params, model_static = eqx.partition(model, filter_spec)
-    (value, state), grads = eqx.filter_value_and_grad(mse_loss, has_aux=True)(model, state, inputs, targets, key)
+def make_step(model, filter_spec, inputs, targets, state, opt, opt_state, key):
+    model_params, model_static = eqx.partition(model, filter_spec)
+    (value, state), grads = mse_loss(model_params, model_static, state, inputs, targets, key)
     updates, opt_state = opt.update(grads, opt_state, eqx.filter(model, eqx.is_array))
     model = eqx.apply_updates(model, updates)
     return model, state, opt_state, value, grads
 
 def train(
     model, 
+    filter_spec,
     train_loader, 
     train_tensors,
     val_tensors,
     loss_fn, 
     state,
     opt,
+    opt_state,
     epochs,
     log_every,
     key=random.PRNGKey(0),
     wandb_run_name=None
 ):
-    if wandb_run_name is not None:
-        wandb.init(
-            project="foundational_ssm_nlb",
-            name=wandb_run_name,
-            config=conf
-        )
     
-    opt_state = opt.init(eqx.filter(model, eqx.is_inexact_array))
-        
     for epoch in range(epochs):
         for inputs, targets in train_loader:
             key, subkey = random.split(key)
             
             model, state, opt_state, loss_value, grads = make_step(
                 model,
+                filter_spec,
                 inputs, 
                 targets,
                 state,
@@ -213,7 +196,6 @@ def train(
             if wandb_run_name is not None:
                 wandb_log_dict = {
                     "epoch": epoch,
-                    # "metrics/train.mse": float(train_mse),
                     "metrics/train.r2": float(train_r2),
                     "metrics/val.r2": float(val_r2),
                 }
@@ -227,114 +209,107 @@ def train(
 # Main script starts here
 # ============================================================
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-processed_data_folder = '/cs/student/projects1/ml/2024/mlaimon/data/foundational_ssm/processed/nlb' 
-dataset_name = "mc_maze"
-processed_data_path = os.path.join(processed_data_folder,dataset_name + ".h5")
-trial_info_path = os.path.join(processed_data_folder,dataset_name + ".csv")
+@hydra.main(config_path="../configs", config_name="s5_maze")
+def main(cfg: DictConfig):
+    print(OmegaConf.to_yaml(cfg))  # For debugging
 
-conf = {
-    'task':'decoding',
-    'dataset': {
-        'dataset': dataset_name
-    },
-    'model': {
-        'input_dim': 182,
-        'output_dim': 2,
-        'd_state': 64,
-        'num_layers': 2,
-        'hidden_dim': 64,
-        'dropout': 0.1,
-        'ssm_core':'s5'
-    },
-    'optimizer': {
-        'lr': 0.001,
-        'weight_decay': 0.01  # Added common parameter
-    },
-    'training': {
-        'batch_size': 64,
-        'epochs': 2000
-    },
-    'device': 'cuda',
-    'framework': 'jax'
-}
+    processed_data_path = os.path.join(
+        to_absolute_path(cfg.dataset.processed_data_folder),
+        cfg.dataset.name + ".h5"
+    )
+    trial_info_path = os.path.join(
+        to_absolute_path(cfg.dataset.processed_data_folder),
+        cfg.dataset.name + ".csv"
+    )
 
-args = OmegaConf.create(conf)
+    wandb.init(
+        project=cfg.wandb_project,
+        name=f"nlb_{cfg.task}_{cfg.model.ssm_core}_l{cfg.model.num_layers}_d{cfg.model.d_state}",
+        config= OmegaConf.to_container(cfg, resolve=True)
+    )
 
-with h5py.File(processed_data_path, 'r') as h5file:
-    dataset_dict = h5_to_dict(h5file)
+    with h5py.File(processed_data_path, 'r') as h5file:
+        dataset_dict = h5_to_dict(h5file)
 
-trial_info = pd.read_csv(trial_info_path)
-trial_info = trial_info[trial_info['split'].isin(['train','val'])]
-min_idx = trial_info['trial_id'].min()
-trial_info['trial_id'] = trial_info['trial_id'] - min_idx
+    trial_info = pd.read_csv(trial_info_path)
+    trial_info = trial_info[trial_info['split'].isin(['train','val'])]
+    min_idx = trial_info['trial_id'].min()
+    trial_info['trial_id'] = trial_info['trial_id'] - min_idx
 
-train_ids = trial_info[trial_info['split']=='train']['trial_id'].tolist()
-val_ids = trial_info[trial_info['split']=='val']['trial_id'].tolist()
+    train_ids = trial_info[trial_info['split']=='train']['trial_id'].tolist()
+    val_ids = trial_info[trial_info['split']=='val']['trial_id'].tolist()
 
-# Concatenate both heldin and heldout spikes since we're using spikes to predict behavior
-spikes = np.concat([
-    dataset_dict['train_spikes_heldin'], 
-    dataset_dict['train_spikes_heldout']],axis=2) 
-smoothed_spikes = torch.tensor(
-    smooth_spikes(spikes, kern_sd_ms=40, bin_width=5), 
-    dtype=torch.float64)
-behavior = torch.tensor(
-    dataset_dict['train_behavior'],
-    dtype=torch.float64)
+    # Concatenate both heldin and heldout spikes since we're using spikes to predict behavior
+    spikes = np.concat([
+        dataset_dict['train_spikes_heldin'], 
+        dataset_dict['train_spikes_heldout']],axis=2) 
+    smoothed_spikes = torch.tensor(
+        smooth_spikes(spikes, kern_sd_ms=40, bin_width=5), 
+        dtype=torch.float64)
+    behavior = torch.tensor(
+        dataset_dict['train_behavior'],
+        dtype=torch.float64)
 
-input_dim = smoothed_spikes.shape[2]
-output_dim = behavior.shape[2]
-
-# Split train and val based on splits from nlb
-train_dataset = TensorDataset(smoothed_spikes[train_ids], behavior[train_ids])
-val_dataset = TensorDataset(smoothed_spikes[val_ids], behavior[val_ids])
-full_dataset = TensorDataset(smoothed_spikes, behavior)
+    # Split train and val based on splits from nlb
+    train_dataset = TensorDataset(smoothed_spikes[train_ids], behavior[train_ids])
+    val_dataset = TensorDataset(smoothed_spikes[val_ids], behavior[val_ids])
 
 
-train_loader = DataLoader(
-    train_dataset, 
-    batch_size=args.training.batch_size, 
-    shuffle=True,
-    collate_fn=numpy_collate
-)
-
-wandb_run_name = f"nlb_{args.task}_{args.model.ssm_core}_l{args.model.num_layers}_d{args.model.d_state}"
-
-model_key = random.PRNGKey(0)
-model = S5(
-    key= model_key,
-    num_blocks=args.model.num_layers,
-    N=args.model.input_dim,
-    ssm_size=args.model.d_state,
-    ssm_blocks=1,
-    H=args.model.hidden_dim,
-    output_dim=args.model.output_dim,
-    clip_eigs=True,
+    train_loader = DataLoader(
+        train_dataset, 
+        batch_size=cfg.training.batch_size, 
+        shuffle=True,
+        collate_fn=numpy_collate
+    )
     
-)
-# model = RNN(in_size=182, out_size=2, hidden_size=128, key=model_key)
+    key = random.PRNGKey(cfg.rng_seed)
+    model_key, train_key = random.split(key, 2)
+    model = S5(
+        key= model_key,
+        num_blocks=cfg.model.num_layers,
+        N=cfg.model.input_dim,
+        ssm_size=cfg.model.d_state,
+        ssm_blocks=1,
+        H=cfg.model.hidden_dim,
+        output_dim=cfg.model.output_dim,
+        clip_eigs=True,
+        
+    )
+    state = eqx.nn.State(model)
+    
+    # Filter spec for freezing parts of the model
+    filter_spec = tree_map(eqx.is_inexact_array, model)
+    
+    if cfg.model.freeze_ssm:
+        where = lambda fs: tuple(block.ssm for block in fs.blocks)
+        filter_spec = eqx.tree_at(where, filter_spec, replace=(False,) * len(filter_spec.blocks))
+    if cfg.model.freeze_mlp:
+        where = lambda m: (m.linear_layer, m.linear_encoder)
+        filter_spec = eqx.tree_at(where, filter_spec, replace=(False, False))
+    
+    opt = optax.adamw(
+        learning_rate=cfg.optimizer.lr, 
+        weight_decay=cfg.optimizer.weight_decay
+    )
+    opt_state = opt.init(eqx.filter(model, filter_spec))
+    
+    loss_fn = mse_loss
 
-# Initialize the state
-state = eqx.nn.State(model)
-batch_size = smoothed_spikes.shape[0]
-keys = random.split(random.PRNGKey(0), batch_size)
+    model = train(
+        model, 
+        filter_spec, 
+        train_loader,
+        train_dataset.tensors, 
+        val_dataset.tensors,  # No validation loader in this example
+        loss_fn,
+        state, 
+        opt,
+        opt_state,
+        epochs=cfg.training.epochs,
+        log_every=10,
+        key=train_key,
+        wandb_run_name=f"nlb_{cfg.task}_{cfg.model.ssm_core}_l{cfg.model.num_layers}_d{cfg.model.d_state}"
+    )
 
-opt = optax.adamw(
-    learning_rate=args.optimizer.lr, 
-    weight_decay=args.optimizer.weight_decay
-)
-loss_fn = mse_loss
-
-model = train(
-    model, 
-    train_loader,
-    train_dataset.tensors, 
-    val_dataset.tensors,  # No validation loader in this example
-    loss_fn,
-    state, 
-    opt,
-    epochs=args.training.epochs,
-    log_every=10,
-    wandb_run_name=wandb_run_name
-)
+if __name__ == "__main__":
+    main()
